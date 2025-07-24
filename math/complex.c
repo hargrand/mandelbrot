8a0d2d4af4a94d452812781d6f12ba1c65a78fe9
Kris Krueger <kris64aom@yahoo.com>
2025-07-23 22:52:15 -0400

    kris_branch - Edited complex.c structs accordingly

--- math/complex.c
+++ math/complex.c
@@ -2,28 +2,28 @@
 
 struct complex add(struct complex x, struct complex y)
 {
-	struct complex return_value = { 0.0, 0.0 };
+	struct complex return_value = { x.re + y.re, x.im + y.im };
 
 	return return_value;
 }
 
 struct complex sub(struct complex x, struct complex y)
 {
-	struct complex return_value = { 0.0, 0.0 };
+	struct complex return_value = { x.re - y.re, x.im - y.im };
 
 	return return_value;
 }
 
 struct complex mul(struct complex x, struct complex y)
 {
-	struct complex return_value = { 0.0, 0.0 };
+	struct complex return_value = { x.re * y.re, x.im * y.im };
 
 	return return_value;
 }
 
 struct complex div(struct complex x, struct complex y)
 {
-	struct complex return_value = { 0.0, 0.0 };
+	struct complex return_value = { x.re / y.re, x.im / y.im };
 
 	return return_value;
 }
